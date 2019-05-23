b538e1c9a0b4bef9eddb07a06df96741b076285a
Aitor1458477 <50233114+Aitor1458477@users.noreply.github.com>
2019-05-23 13:13:43 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -33,10 +33,10 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-     	   //Automatico();
+     	   Automatico();
      	   
             app.push = PushNotification.init({
-            	alert("hola");
+            	//alert("hola");
 	           "android": {
 	               "senderID": "405185187585"
 	           },
@@ -48,7 +48,7 @@ var app = {
 	           "windows": {}
 	       });
 
-       app.push.on('registration', function(data) {
+       /*app.push.on('registration', function(data) {
            //console.log("registration event: " + data.registrationId);
            //document.getElementById("regId").innerHTML = data.registrationId;
            alert("hola");
@@ -66,7 +66,7 @@ var app = {
 			   xmlhttp.send();
 
            }
-       });
+       });*/
 
        app.push.on('error', function(e) {
            console.log("push error = " + e.message);
@@ -180,6 +180,25 @@ function Automatico(){
 
 		window.open("https://siesoluciones.com/tickets2/movil/index2.php?usuario="+user+"&clave="+pass, "_blank", "location=no");
 	}
+	 app.push.on('registration', function(data) {
+           //console.log("registration event: " + data.registrationId);
+           //document.getElementById("regId").innerHTML = data.registrationId;
+           alert("hola");
+           alert(data.registrationId);
+           var oldRegId = localStorage.getItem('registrationId');
+           var id = localStorage.getItem('Id');
+           var xmlhttp=new XMLHttpRequest();
+           var urlToken="https://siesoluciones.com/tickets2/movil/ajaxGuardarToken.php?idUsu="+id+"&token="+oldRegId;
+
+           if (oldRegId != data.registrationId || id!=null ) {
+               // Save new registration ID
+               localStorage.setItem('registrationId', data.registrationId);
+               // Post registrationId to your app server as the value has changed
+               xmlhttp.open("GET",urlToken,true);
+			   xmlhttp.send();
+
+           }
+
 };
 
 
