225f73335874da6f13bb560711add9927b09664c
Aitor1458477 <50233114+Aitor1458477@users.noreply.github.com>
2019-05-23 12:08:23 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -33,9 +33,10 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-     	    Automatico();
+     	   // Automatico();
 
             app.push = PushNotification.init({
+            alert("hola");
 	           "android": {
 	               "senderID": "405185187585"
 	           },
